786e1a85e8a2934664074271a1b41b793bb04b06
Jwright707 <40744027+Jwright707@users.noreply.github.com>
2019-04-26 20:57:31 -0700

    Updated home/res

--- src/components/home.js
+++ src/components/home.js
@@ -24,7 +24,9 @@ class Home extends Component {
             <p className="skills">
               <b>Productive: </b> Python, R, HTML, CSS, ReactJS
               <br />
-              <b>Familiar: </b> Java, C++, JavaScript, Django, SQL, WordPress
+              <b>Familiar: </b> Java, C++, JavaScript, Django, SQL, WordPress,
+              Nginx, PHP with Laravel, PostgreSQL, Redis, Node.js with
+              Express.js, Bash
               <br />
               <b>Software Development: </b> Git/Github, Travis CI, Agile,
               Apache, Docker, Junit, Google Cloud
